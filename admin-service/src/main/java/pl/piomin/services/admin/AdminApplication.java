c438d8c7ef900e02e13c78216adbe7e914ed85fb
Piotr Mińkowski <piotr_minkowski@wp.pl>
2020-02-17 00:30:42 +0100

    final changes for spring boot admin

--- admin-service/src/main/java/pl/piomin/services/admin/AdminApplication.java
+++ admin-service/src/main/java/pl/piomin/services/admin/AdminApplication.java
@@ -5,10 +5,12 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
+@EnableScheduling
 public class AdminApplication {
 
 	public static void main(String[] args) {
