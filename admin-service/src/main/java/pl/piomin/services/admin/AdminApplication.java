e5c08f984c724bfd87cac72aa6e76f4d7d39ad31
Piotr Mińkowski <piotr_minkowski@wp.pl>
2020-02-16 12:18:03 +0100

    adding spring boot admin

---
+++ admin-service/src/main/java/pl/piomin/services/admin/AdminApplication.java
@@ -0,0 +1,18 @@
+package pl.piomin.services.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+}
