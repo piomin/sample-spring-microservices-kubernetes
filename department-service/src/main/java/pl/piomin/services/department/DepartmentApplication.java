35218b426aac42174837201ca9b5f8cfd04764d6
piomin <piotr.minkowski@gmail.com>
2020-09-10 00:11:41 +0200

    Adding a circuit breaker configuration

--- department-service/src/main/java/pl/piomin/services/department/DepartmentApplication.java
+++ department-service/src/main/java/pl/piomin/services/department/DepartmentApplication.java
@@ -1,7 +1,12 @@
 package pl.piomin.services.department;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,6 +19,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -40,6 +47,22 @@ public class DepartmentApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id ->
+                new Resilience4JConfigBuilder(id)
+                        .timeLimiterConfig(TimeLimiterConfig.custom()
+                                .timeoutDuration(Duration.ofMillis(500))
+                                .build())
+                        .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                                .slidingWindowSize(10)
+                                .failureRateThreshold(66.6F)
+                                .slowCallRateThreshold(66.6F)
+                                .build())
+                        .build()
+        );
     }
 
 }
