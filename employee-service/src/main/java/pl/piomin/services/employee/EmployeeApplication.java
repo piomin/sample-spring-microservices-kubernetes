74c49b5247b30fdbf6398740ea1f0e8bd4a91715
Ming Jin <skyairmj@gmail.com>
2023-08-05 19:08:25 +0800

    upgrade k8s ingress API version & aggregate microservice API with Springdoc OpenAPI

--- employee-service/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
+++ employee-service/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
@@ -1,5 +1,7 @@
 package pl.piomin.services.employee;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +10,9 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableMongoRepositories
+@OpenAPIDefinition(info =
+    @Info(title = "Employee API", version = "1.0", description = "Documentation Employee API v1.0")
+)
 public class EmployeeApplication {
 
     public static void main(String[] args) {
--- gateway-service/src/main/java/pl/piomin/services/gateway/api/GatewayApi.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/api/GatewayApi.java
@@ -1,13 +1,31 @@
 package pl.piomin.services.gateway.api;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class GatewayApi {
 
 	@Autowired
 	RouteDefinitionLocator locator;
+
+	@Bean
+	public List<GroupedOpenApi> apis() {
+		List<GroupedOpenApi> groups = new ArrayList<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		assert definitions != null;
+		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+			String name = routeDefinition.getId().replaceAll("-service", "");
+			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+		});
+		return groups;
+	}
 
 }
