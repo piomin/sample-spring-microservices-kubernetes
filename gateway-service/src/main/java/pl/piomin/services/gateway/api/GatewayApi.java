71115fe7c59833ee7f082a6beaf9436646a474e9
piomin <piotr.minkowski@gmail.com>
2023-10-08 01:43:42 +0200

    #43 Fixing OpenAPI

--- gateway-service/src/main/java/pl/piomin/services/gateway/api/GatewayApi.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/api/GatewayApi.java
@@ -1,8 +1,14 @@
 package pl.piomin.services.gateway.api;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class GatewayApi {
@@ -10,4 +16,16 @@ public class GatewayApi {
 	@Autowired
 	RouteDefinitionLocator locator;
 
+	@Bean
+	public List<GroupedOpenApi> apis() {
+		List<GroupedOpenApi> groups = new ArrayList<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		assert definitions != null;
+		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches("employee|department|organization"))
+				.forEach(routeDefinition -> {
+			String name = routeDefinition.getId();
+			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + routeDefinition.getId() + "/**").group(name).build());
+		});
+		return groups;
+	}
 }
